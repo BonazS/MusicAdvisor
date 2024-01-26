8894eee67367b9c1616b6bf30a72d5748a725ce3
SIMONE <simone.bonasegale@gmail.com>
2024-01-26 18:23:45 +0100

    finish stage 3 with thread main waiting authorization code

--- Music Advisor/task/src/http/Client.java
+++ Music Advisor/task/src/http/Client.java
@@ -15,18 +15,17 @@ public class Client {
     private final String CLIENT_URI = "http://localhost:8080";
     private static final String SPOTIFY_CLIENT_ID = "2f48afb9911d4d9baf554c513843c264";
     private static final String SPOTIFY_CLIENT_SECRET = "9fbb9e072d9c46b392a046e6f7770772";
-    private final String SPOTIFY_AUTH_LINK = String.format(
+    private String spotifyAuthLink = String.format(
             "%s/authorize?client_id=%s&redirect_uri=%s&response_type=code",
             spotifyAccessPoint,
             SPOTIFY_CLIENT_ID,
             CLIENT_URI
     );
-    private final String SPOTIFY_ACCESS_TOKEN_LINK = String.format("%s/api/token", spotifyAccessPoint);
-    private static final int AUTH_TIMEOUT_MS = 10000;
+    private String spotifyAccessTokenLink = String.format(
+            "%s/api/token", spotifyAccessPoint);
     private static final int ACCESS_TOKEN_TIMEOUT_MS = 10000;
     private String spotifyAuthCode;
     private String spotifyAccessToken;
-    private String jsonData = "";
 
     public Client() {
         client = HttpClient.newBuilder().build();
@@ -34,10 +33,12 @@ public class Client {
 
     public Client(String spotifyAccessPoint) {
         this();
+        this.spotifyAuthLink = spotifyAuthLink.replace(this.spotifyAccessPoint, spotifyAccessPoint);
+        this.spotifyAccessTokenLink = spotifyAccessTokenLink.replace(this.spotifyAccessPoint, spotifyAccessPoint);
         this.spotifyAccessPoint = spotifyAccessPoint;
     }
 
-    public String getSpotifyAuthLink() { return SPOTIFY_AUTH_LINK; }
+    public String getSpotifyAuthLink() { return spotifyAuthLink; }
 
     public String getSpotifyAuthCode() { return spotifyAuthCode; }
 
@@ -47,27 +48,14 @@ public class Client {
 
     public String getAccessToken() { return spotifyAccessToken; }
 
-    public HttpResponse<String> sendSpotifyAuthorization() throws IOException, InterruptedException {
-        System.out.println("waiting for code...");
-        HttpRequest spotifyAuthRequest = HttpRequest.newBuilder()
-                .uri(URI.create(SPOTIFY_AUTH_LINK))
-                .timeout(Duration.ofMillis(AUTH_TIMEOUT_MS))
-                .build();
-        HttpResponse<String> response = clientSendRequest(spotifyAuthRequest);
-        System.out.println(response.statusCode());
-        System.out.println(response.body());
-        return response;
-    }
-
     public HttpResponse<String> sendSpotifyAccessToken() throws IOException, InterruptedException {
-        // System.out.println("making http request for access_token...");
         HttpRequest spotifyAccessTokenRequest = HttpRequest.newBuilder()
                 .header("Content-Type",
                         "application/x-www-form-urlencoded")
                 .header("Authorization",
                         "Basic " + Base64.getEncoder()
                                 .encodeToString("%s:%s".formatted(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET).getBytes()))
-                .uri(URI.create(SPOTIFY_ACCESS_TOKEN_LINK))
+                .uri(URI.create(spotifyAccessTokenLink))
                 .POST(HttpRequest.BodyPublishers.ofString(
                         String.format(
                                 "code=%s&redirect_uri=%s&grant_type=authorization_code",
@@ -78,20 +66,6 @@ public class Client {
                 .timeout(Duration.ofMillis(ACCESS_TOKEN_TIMEOUT_MS))
                 .build();
         return clientSendRequest(spotifyAccessTokenRequest);
-    }
-
-    public HttpRequest clientGETRequest() {
-        return HttpRequest.newBuilder()
-                .uri(URI.create(CLIENT_URI))
-                .build();
-    }
-
-    public HttpRequest clientPOSTRequest() {
-        return HttpRequest.newBuilder()
-                .header("Content-Type", "application/json")
-                .uri(URI.create(CLIENT_URI))
-                .POST(HttpRequest.BodyPublishers.ofString(jsonData))
-                .build();
     }
 
     public HttpResponse<String> clientSendRequest(HttpRequest request) throws IOException, InterruptedException {
